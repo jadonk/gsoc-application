81b1a93aa4bc9f7f92b61e3a9e4518a306159708
Anuj Deshpande <anujdeshpande92@gmail.com>
2013-04-20 12:50:49 +0530

    changed name to Anuj Deshpande

--- ExampleEntryJasonKridner/helloworld.c
+++ ExampleEntryJasonKridner/helloworld.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/*
+Demo run for GSoC '13
+ */
 
 int main(int argc, char** argv)
 {
-	printf("Jason Kridner - %s\n", __DATE__);
+  /* 
+     printf("Jason Kridner - %s\n", __DATE__);
+  */
+  printf("Anuj Deshpande - %s \n", __DATE__);
 }
