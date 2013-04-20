56acd388d6455dc424ffbce4e51e3e22548a6941
Emilio López <emilio@elopez.com.ar>
2013-04-20 16:05:53 -0300

    Modify helloworld.c to print my name next to the current day

--- EmilioLopez/helloworld.c
+++ EmilioLopez/helloworld.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
-	printf("Jason Kridner - %s\n", __DATE__);
+	time_t now;
+	struct tm *today;
+	char strdate[50];
+
+	time(&now);
+	today = localtime(&now);
+
+	strftime(strdate, sizeof(strdate), "%d-%m-%Y", today);
+	printf("Emilio Lopez - %s\n", strdate);
 }
