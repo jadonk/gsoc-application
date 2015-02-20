6374e4e4d7c401446c3becb457c45dea0de61287
Satyam Zode <satyamzode@gmail.com>
2015-02-21 00:51:41 +0530

    Changed name to my name

--- ExampleEntryJasonKridner/helloworld.c
+++ ExampleEntryJasonKridner/helloworld.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/*
+	GSoC application 2015 Requirement demo.
+*/
+
 int main(int argc, char** argv)
 {
+	/*
+
 	printf("Jason Kridner - %s\n", __DATE__);
+
+	*/
+	
+	printf("Satyam Zode - %s\n", __DATE__);
 }
