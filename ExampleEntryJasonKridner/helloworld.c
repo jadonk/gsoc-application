ee5b14d54beda2f4dbc17a210f461b5d0d3baf96
Belinda Brown <belindabrownr04@gmail.com>
2018-03-22 11:50:07 -0600

    GSOC 2018: Cross compilaion Task

--- ExampleEntryJasonKridner/helloworld.c
+++ ExampleEntryJasonKridner/helloworld.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <time.h>
 
-int main(int argc, char** argv)
+int main()
+
 {
-	printf("Jason Kridner - %s\n", __DATE__);
+	printf("Hello World!\n");
+	char  name[16];
+	printf("Hey there, what is your name?\n");
+	scanf("%s", name);
+	printf("Hello %s\n", name);
+	time_t t = time(NULL);
+	struct tm *tm = localtime(&t);
+	printf("The date based on your timezone is %s\n", asctime(tm));
+
 }
